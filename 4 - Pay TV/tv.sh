85a2159f6b59789b1646c18e450b4002de0b9584
Conan <conand@gmail.com>
2013-10-23 22:15:34 -0700

    / ‘4 - Pay TV/tv.sh’

--- 4 - Pay TV/tv.sh
+++ 4 - Pay TV/tv.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # start of pass is AXM
+# sh tv.sh char
 cat $1 |while read line;do
 	echo $line >> output
 	curl --data key=$2$line\&debug https://ctf.fluxfingers.net:1316/gimmetv >> output
